78ddd00a875a30c4e375aa9076a9de1c37c4f523
asif.haque <asif.haque@technonext.com>
2025-07-01 00:40:31 +0600

    Previous User API Integrated.

--- AndroidJetcakComposeMVIHiltPagination/app/src/main/java/com/technonext/androidjetcakcomposemvihiltpagination/domain/repository/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.technonext.androidjetcakcomposemvihiltpagination.domain.repository
-
-import com.technonext.androidjetcakcomposemvihiltpagination.domain.model.User
-
-interface UserRepository {
-    suspend fun getUsers(): Result<List<User>>
-}
-
